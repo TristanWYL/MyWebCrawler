1db7274f8fa04dacfc9290253ad4645460c005ba
TristanWYL <tristan_wyl@hotmail.com>
2020-09-17 17:38:54 +0800

    temporary saving

--- preprocess.py
+++ preprocess.py
@@ -56,6 +56,7 @@ def data_merge(main_dict, word_dict, detail_dict):
 
 
 def canclid(file_path):
+    return
     data = dict()
     data_source_id = file_name_des = inspect.stack()[0][3] +"_"+ file_path.split(os.sep)[-1]
     # create output directory
@@ -68,6 +69,7 @@ def canclid(file_path):
         remove_percent_sign = False
         remove_last_column = False
         extract_third_column_as_jyutping = False
+        start_position = 1
         if file_name_src == "jp_table_char_dict.yaml":
             remove_last_column = True
             start_position = 12
@@ -90,10 +92,10 @@ def canclid(file_path):
                     continue
                 if l.startswith("#"):
                     continue
-                items = l.split(" ")
+                items = l.split()
                 items = [item.strip() for item in items]
                 # prepare the word instance
-                word_dict = EmptyDict(word_pattern)
+                word_dict = EmptyDict(**word_pattern)
                 word_dict["word"] = items[0]
                 jyutping = " ".join(items[1:])
                 if remove_last_column:
@@ -106,7 +108,7 @@ def canclid(file_path):
                 word_dict["jyutping"] = jyutping
                 word_dict["jyutping_mode"] = 6
                 # prepare the detail instance
-                detail_dict = EmptyDict(detail_pattern)
+                detail_dict = EmptyDict(**detail_pattern)
                 detail_dict["source"] = data_source_id
                 # merge data
                 data_merge(data, word_dict, detail_dict)
@@ -115,64 +117,110 @@ def canclid(file_path):
             with open(file_path, 'r') as f:
                 reader = csv.DictReader(f)
                 for row in reader:
-                    word_dict = EmptyDict(word_pattern)
+                    word_dict = EmptyDict(**word_pattern)
                     word_dict["word"] = row["粵字"].strip()
                     word_dict["jyutping"] = row["粵拼"].strip()
                     word_dict["jyutping_mode"] = 6
-                    detail_dict = EmptyDict(detail_pattern)
+                    detail_dict = EmptyDict(**detail_pattern)
                     detail_dict["source"] = data_source_id
                     word_with_same_jyutping = row["同音字"].strip()
                     if word_with_same_jyutping != "/": 
                         detail_dict["words_with_similar_jyutping"] = word_with_same_jyutping
                     detail_dict["example_Chinese"] = row["簡略解釋與例子"].strip()
+                    data_merge(data, word_dict, detail_dict)
         elif file_name_src == "can_orth_draft.csv":
             with open(file_path, 'r') as f:
                 reader = csv.DictReader(f)
                 for row in reader:
-                    word_dict = EmptyDict(word_pattern)
+                    word_dict = EmptyDict(**word_pattern)
                     word_dict["word"] = row["粵研社（粵語協會）"].strip()
                     word_dict["jyutping"] = row["粵拼"].strip()
                     word_dict["jyutping_mode"] = 6
-                    detail_dict = EmptyDict(detail_pattern)
+                    detail_dict = EmptyDict(**detail_pattern)
                     detail_dict["source"] = data_source_id
                     detail_dict["example_Chinese"] = row["意思及例子"].strip()
+                    data_merge(data, word_dict, detail_dict)
         elif file_name_src == "can_orth_houxingquan.csv":
             with open(file_path, 'r') as f:
                 reader = csv.DictReader(f)
                 for row in reader:
-                    word_dict = EmptyDict(word_pattern)
+                    word_dict = EmptyDict(**word_pattern)
                     word_dict["word"] = row["推荐字"].strip()
-                    word_dict["jyutping"] = row["字音（粤拼）"].strip()
+                    word_dict["jyutping"] = row["\ufeff字音（粤拼）"].strip()
                     word_dict["jyutping_mode"] = 6
-                    detail_dict = EmptyDict(detail_pattern)
+                    detail_dict = EmptyDict(**detail_pattern)
                     detail_dict["source"] = data_source_id
                     detail_dict["words_with_similar_meaning"] = row["异体字"].strip()
                     detail_dict["words_with_similar_jyutping"] = row["同/近音字 (潜在异体字）"].strip()
                     detail_dict["explanation_Chinese"] = row["字（词）义"].strip()
+                    data_merge(data, word_dict, detail_dict)
         elif file_name_src == "can_orth_main.csv":
             with open(file_path, 'r') as f:
                 reader = csv.DictReader(f)
                 for row in reader:
-                    word_dict = EmptyDict(word_pattern)
+                    word_dict = EmptyDict(**word_pattern)
                     word_dict["word"] = row["通用粵字"].strip()
                     word_dict["jyutping"] = row["學會粵拼"].strip()
                     word_dict["jyutping_mode"] = 6
-                    detail_dict = EmptyDict(detail_pattern)
+                    detail_dict = EmptyDict(**detail_pattern)
                     detail_dict["source"] = data_source_id
                     detail_dict["example_Chinese"] = row["意思及例子"].strip()
                     detail_dict["remark"] = row["疑問或建議/註解"].strip()
+                    data_merge(data, word_dict, detail_dict)
         elif file_name_src == "can_orth_research_group.csv":
             with open(file_path, 'r') as f:
                 reader = csv.DictReader(f)
                 for row in reader:
-                    word_dict = EmptyDict(word_pattern)
+                    word_dict = EmptyDict(**word_pattern)
                     word_dict["word"] = row["通用粵字"].strip()
                     word_dict["jyutping"] = row["學會粵拼"].strip()
                     word_dict["jyutping_mode"] = 6
-                    detail_dict = EmptyDict(detail_pattern)
+                    detail_dict = EmptyDict(**detail_pattern)
                     detail_dict["source"] = data_source_id
                     detail_dict["example_Chinese"] = row["意思及例子"].strip()
                     detail_dict["remark"] = row["疑問或建議/註解"].strip()
+                    data_merge(data, word_dict, detail_dict)
+        elif file_name_src == "can_orth_yueyinxiaojing_general.csv":
+            with open(file_path, 'r') as f:
+                reader = csv.DictReader(f)
+                for row in reader:
+                    word_dict = EmptyDict(**word_pattern)
+                    word_dict["word"] = row["字頭"].strip()
+                    word_dict["jyutping"] = row["粵-聲韻調"].strip()
+                    word_dict["jyutping_mode"] = '6*'
+                    detail_dict = EmptyDict(**detail_pattern)
+                    detail_dict["source"] = data_source_id
+                    detail_dict["remark"] = row["釋義補充"].strip()
+                    detail_dict["explanation_Chinese"] = row["廣韻釋義"].strip()
+                    data_merge(data, word_dict, detail_dict)
+        elif file_name_src == "can_orth_yueyinxiaojing_words_with_same_jyutping.csv":
+            with open(file_path, 'r') as f:
+                reader = csv.DictReader(f)
+                for row in reader:
+                    words_with_same_reading = row["粵-同音字"].strip()
+                    for w in words_with_same_reading:
+                        word_dict = EmptyDict(**word_pattern)
+                        word_dict["word"] = w
+                        word_dict["jyutping"] = row["粵-聲韻調"].strip()
+                        word_dict["jyutping_mode"] = '6*'
+                        detail_dict = EmptyDict(**detail_pattern)
+                        detail_dict["source"] = data_source_id
+                        detail_dict["words_with_similar_jyutping"] = words_with_same_reading.replace(w, "")
+                        data_merge(data, word_dict, detail_dict)
+        elif file_name_src == "can_orth_yueyinxiaojing.csv":
+            with open(file_path, 'r') as f:
+                reader = csv.DictReader(f)
+                for row in reader:
+                    words_with_same_reading = row["粵京對應"].strip()
+                    for w in words_with_same_reading:
+                        word_dict = EmptyDict(**word_pattern)
+                        word_dict["word"] = w
+                        word_dict["jyutping"] = row["粵-聲韻調"].strip()
+                        word_dict["jyutping_mode"] = '6*'
+                        detail_dict = EmptyDict(**detail_pattern)
+                        detail_dict["source"] = data_source_id
+                        detail_dict["words_with_similar_jyutping"] = words_with_same_reading.replace(w, "")
+                        data_merge(data, word_dict, detail_dict)
     # write the data into a json file
     with open(output_file_name, 'w', encoding='utf-8') as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
@@ -211,12 +259,12 @@ def hkcc(file_path):
                 continue
             word_jyutping_added.append(word_jyutping)
             # prepare the word instance
-            word_dict = EmptyDict(word_pattern)
+            word_dict = EmptyDict(**word_pattern)
             word_dict["word"] = word
             word_dict["jyutping"] = jyutping
             word_dict["jyutping_mode"] = 6
             # prepare the detail instance
-            detail_dict = EmptyDict(detail_pattern)
+            detail_dict = EmptyDict(**detail_pattern)
             detail_dict["source"] = data_source_id
             detail_dict["example_Chinese"] = words
             # merge data
